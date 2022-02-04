a9475e33c5c643ae19acd5efd5a90cf7ba2cc6fe
Werner Zimmer <werner.zimmer@outlook.com>
2022-02-04 20:10:05 +0100

    Theme Quellen hinzugefügt. Post-Build aktiviert.

---
+++ PostBuildAction.ps1
@@ -0,0 +1,19 @@
+﻿# Parameter
+$projectDir = $args[0]
+Write-Host "projectDir: " $projectDir
+$projectName = $args[1]
+Write-Host "projectName: " $projectName
+$targetDir = $args[2]
+Write-Host "targetDir: " $targetDir
+$targetName = $args[3]
+Write-Host "targetName: " $targetName
+# Zielordner
+$ThemeDir = "wwwroot\Themes\"
+$releaseName = "oqtane.framework-3.0.2"
+$releaseProject = "Oqtane.Server"
+$releaseProjectDir = $projectDir.Replace($projectName + "\", "") + $releaseName + "\" + $releaseProject
+Write-Host "releaseProjectDir: " $releaseProjectDir
+# Kopiervorgang
+Copy-Item ($targetDir + $targetName + ".dll") -Destination ($releaseProjectDir + "\" + $targetDir.Replace($projectDir, ""))
+Copy-Item ($targetDir + $targetName + ".pdb") -Destination ($releaseProjectDir + "\" + $targetDir.Replace($projectDir, ""))
+Copy-Item ($projectDir + $ThemeDir + $projectName) -Force -Recurse -Destination ($releaseProjectDir + "\" + $ThemeDir)
---
+++ WszTheme/ThemeInfo.cs
@@ -0,0 +1,20 @@
+﻿using Oqtane.Models;
+using Oqtane.Themes;
+
+namespace Wsz.Oqt.WszTheme
+{
+    public class ThemeInfo : ITheme
+    {
+        public Theme Theme => new Theme
+        {
+            Name = "Wettkampf-Software Theme",
+            Version = "1.0.1",
+            PackageName = "Wsz.Oqt.WszTheme",
+            Owner = "Wettkampf-Software Zimmer",
+            Url = "https://www.wettkampf-software-zimmer.de/",
+            Contact = "info2021@wettkampf-software-zimmer.de",
+            License = "Free",
+        };
+
+    }
+}
