0deb931452cd8e0ed56ed09584cd6db2466c1794
Werner Zimmer <werner.zimmer@outlook.com>
2023-06-06 17:04:19 +0200

    Script auf Framework Version 3.4.3 geändert.

--- PostBuildAction.ps1
+++ PostBuildAction.ps1
@@ -9,7 +9,7 @@ $targetName = $args[3]
 Write-Host "targetName: " $targetName
 # Zielordner
 $ThemeDir = "wwwroot\Themes\"
-$releaseName = "oqtane.framework-3.0.2"
+$releaseName = "oqtane.framework-3.4.3"
 $releaseProject = "Oqtane.Server"
 $releaseProjectDir = $projectDir.Replace($projectName + "\", "") + $releaseName + "\" + $releaseProject
 Write-Host "releaseProjectDir: " $releaseProjectDir
