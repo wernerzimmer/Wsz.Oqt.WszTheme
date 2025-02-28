f87d73039ec8592e7be360f15389653967f428a3
Werner Zimmer <werner.zimmer@outlook.com>
2025-02-28 12:52:11 +0100

    Version auf 6.1.0 geändert. Bootstrap auf lokale Dateien umgestellt.

--- WszTheme/ThemeInfo.cs
+++ WszTheme/ThemeInfo.cs
@@ -9,7 +9,7 @@ namespace Wsz.Oqt.WszTheme
         public Theme Theme => new Theme
         {
             Name = "Wettkampf-Software Theme",
-            Version = "5.0.2",
+            Version = "6.1.0",
             PackageName = "Wsz.Oqt.WszTheme",
             Owner = "Wettkampf-Software Zimmer",
             Url = "https://www.wettkampf-software-zimmer.de/",
@@ -17,9 +17,12 @@ namespace Wsz.Oqt.WszTheme
             License = "Free",
             Resources = new List<Resource>()
             {
-                new Resource { ResourceType = ResourceType.Stylesheet, Url = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css", Integrity = "sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==", CrossOrigin = "anonymous" },
+		        // Obtained from https://cdnjs.com/libraries
+                // Sourcefile: "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css"
+                new Resource { ResourceType = ResourceType.Stylesheet, Url = "/Themes/Wsz.Oqt.WszTheme/bootstrap.min.css", Integrity = "sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==", CrossOrigin = "anonymous" },
                 new Resource { ResourceType = ResourceType.Stylesheet, Url = "~/WszTheme.css" },
-                new Resource { ResourceType = ResourceType.Script, Url = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js", Integrity = "sha512-VK2zcvntEufaimc+efOYi622VN5ZacdnufnmX7zIhCPmjhKnOi9ZDMtg1/ug5l183f19gG1/cBstPO4D8N/Img==", CrossOrigin = "anonymous" }
+                // Sourcefile: "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js"
+                new Resource { ResourceType = ResourceType.Script, Url = "/Themes/Wsz.Oqt.WszTheme/bootstrap.bundle.min.js", Integrity = "sha512-7Pi/otdlbbCR+LnW+F7PwFcSDJOuUJB3OxtEHbg4vSMvzvJjde4Po1v4BR9Gdc9aXNUNFVUY+SK51wWT8WF0Gg==", CrossOrigin = "anonymous" }
             }
         };
 
